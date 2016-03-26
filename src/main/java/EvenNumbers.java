5c77c79ca09297b91ef8ba8065fe26ae87256b9b
Prathiba Lingappan <prathi@prathis-macbook.gateway.pace.com>
2016-03-26 16:35:12 -0700

    Create Loop Exercise Files

---
+++ src/main/java/EvenNumbers.java
@@ -0,0 +1,39 @@
+/**
+ *   File Name: EvenNumbers.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 26, 2016
+ *
+ */
+
+/**
+ * EvenNumbers //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class EvenNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i < 101; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
